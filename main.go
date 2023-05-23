0d03c8be72bdd13791e95924e90f628893e05b95
Athfan Fasee <aththaar47@gmail.com>
2023-05-23 21:31:20 +0530

    chnages to Hello function

--- main.go
+++ main.go
@@ -2,17 +2,43 @@ package main
 
 import "fmt"
 
+const (
+	spanish = "Spanish"
+	french = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+)
+
 func PrintAthfan() string {
 	return "Athfan"
 }
-func Hello(name string) string {
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return "Hello, " + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	// In our function signature we have made a named return value (prefix string)
+	// This will create a variable called prefix in your function
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return // Naked return
 }
 
 func main() {
 	fmt.Println(PrintAthfan())
-	fmt.Println(Hello("Athfan"))
+	fmt.Println(Hello("Athfan", ""))
 }
--- main_test.go
+++ main_test.go
@@ -12,16 +12,28 @@ func TestPrintAthfan(t *testing.T) {
 func TestHello(t *testing.T) {
 	// Both t.Run here are called Subtests
 	t.Run("saying hello to someone", func(t *testing.T) {
-		got := Hello("Athfan")
+		got := Hello("Athfan", "")
 		want := "Hello, Athfan"
 
 		Equal(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		Equal(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		Equal(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 		Equal(t, got, want)
 	})
 }
